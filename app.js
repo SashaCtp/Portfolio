cd2ec36bc4873ee39b383d3d6ba486526ba8622c
Sasha Contrepois <sasha.contrepois@hotmail.fr>
2020-11-30 23:29:29 +0100

    Update: Add Bootstrap 4 & VueJS, Improve design

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 
+
+/*
+    INIT Middleware
+*/
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 //Static fonts
 app.use('/fonts', express.static(__dirname + '/src/fonts'))
 
@@ -16,10 +23,33 @@ app.use('/pdf', express.static(__dirname + '/src/pdf'))
 //Static javascript
 app.use('/js', express.static(__dirname + '/src/js'))
 
+//Messages
+app.post('/message', (req, res) => {
+
+    res.sendStatus(200);
+
+    if(req.body.name && req.body.email && req.body.message){
+
+        console.log("New message from " + req.body.name);
+    
+    }
+
+});
+
+//Root
 app.get('/', (req, res) => {
-
-    res.sendFile(__dirname + '/index.html')
-
+    res.sendFile(__dirname + '/index.html');
 })
 
-app.listen(8080)
+console.log("🚀 Starting server on port " + PORT)
+try{
+    app.listen(PORT);
+
+    console.log("\n✅ Server started on port " + PORT)
+}catch(e){
+
+    console.log("\n⛔ Server encountered an error. Start sequence aborted.")
+    console.error(e.error);
+
+}
+
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,136 @@
+/*
+    CLIENT
+*/
+
+var app = new Vue({
+    el: '#app',
+    data : {
+        msgWindowOpen : false,
+        skillLists : [
+            [
+                { name :"Javascript",informations: "jQuery, VueJS",},
+                { name: "HTML & CSS", informations: ""},
+                { name: "Java", informations: ""},
+                { name: "Git", informations: "Gitlab, GitHub, AzureDevops"} 
+            ],
+            [
+                { name:"PHP", informations : "Wordpress"},
+                { name:"NodeJS", informations: "" },
+                { name:"SQL", informations : ""},
+                { name:"C/C++", informations : ""}
+            ],
+            [
+                { name: "Bureautique", informations : "Word, Excel, Powerpoint"},
+                { name: "SolidWorks", informations: ""},
+                { name: "SuiteAdobe", informations : "Photoshop, AdobeXD, After Effects"},
+                { name: "Gestion de projet", informations: "Trello, AzureDevops"}
+            ],
+            [
+                { name : "Français", informations: ""},
+                { name: "Anglais", informations: "Cambdrige Certificate"},
+                { name: "Italien", informations: ""},
+                { name: "Russe", informations: "Niveau débutant"}
+            ]
+        ],
+        projectTimeline : [
+            {   
+                year: 2020,
+                projects: [
+                    {
+                        name: "CoinCoin du FPV",
+                        description: "Un site d'achat et de revente de pièces dédiées au FPV.",
+                        imageUrl: "/img/projects/coincoinfpv.png",
+                        specitifactionsUrl : null,
+                        learnMoreUrl: null,
+                        technos : [
+                            "HTML & CSS",
+                            "PHP (WordPress)",
+                            "SQL",
+                            "Javascript",
+                            "Bootstrap"
+                            
+                        ]
+                    },
+                ],
+            },
+            {
+                year: 2019,
+                projects: [
+                    {
+                        name: "AeroSaver",
+                        description: "Projet de fin d'année pour porter secours aux victimes de noyades.",
+                        imageUrl: "/img/projects/aerosaver.png",
+                        specificationsUrl: null,
+                        learnMoreUrl: null,
+                        technos: [
+                            "C++"
+                        ]
+                    },
+                    {
+                        name: "Relax Bot",
+                        description: "Un projet pour prendre en main la librairie Discord.JS.",
+                        imageUrl: "/img/projects/relaxbot.png",
+                        specitifactionsUrl : null,
+                        learnMoreUrl: null,
+                        technos : [
+                            "NodeJS",
+                            "Javascript",
+                            "Discord.JS"
+                        ]
+                    }
+                ]
+
+            },{
+                year : 2017,
+                projects : [
+                    {
+                        name: "Monopo.li",
+                        description: "Un vrai jeu Monopoly sur un navigateur internet !",
+                        imageUrl : "/img/projects/monopoli.png",
+                        specitifactionsUrl: null,
+                        learnMoreUrl: null,
+                        technos: [
+                            "HTML & CSS",
+                            "Javascript",
+                            "VueJS",
+                            "SocketIO",
+                            "NodeJS"
+                        ]
+                    }
+                ]
+
+            },
+            {
+                year: 2016,
+                projects: [
+                    {
+                        name: "RubyzGames",
+                        description: "Un serveur de jeu amusant et innovant.",
+                        imageUrl: "/img/projects/rubyzgames.png",
+                        specitifactionsUrl: null,
+                        learnMoreUrl: null,
+                        technos: [
+                            "Java",
+                            "SQL",
+                            "Bash",
+                            "HTML & CSS",
+                            "Javascript"
+                        ]
+                    }
+
+                ]
+            }
+        ]
+    },
+    methods : {
+        openMsgWindow : function(){
+
+            this.msgWindowOpen = true;
+
+        }
+    }
+});
+
 //Toggle responsive nav
 $('#navButton').click(function(){
     $(this).toggleClass('is-active');
