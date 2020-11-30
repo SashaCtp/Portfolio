e2343cbc34a02980b8b71129d58a1cdb84799e24
Sasha Contrepois <sasha.contrepois@hotmail.fr>
2020-11-30 23:30:32 +0100

    📝 Update : Add changing gradient animation

--- src/js/app.js
+++ src/js/app.js
@@ -150,3 +150,45 @@ $(document).ready(function(){checkHeader();})
 
 //Scroll
 $(window).scroll(function(){checkHeader();});
+
+
+
+    /* Dark color : rgb(54, 55, 149)   , Light color : rgb(0, 92, 151) */
+var darkBlue = [54, 55, 149];
+var lightBlue = [0, 92, 151];
+
+var n = 0, increment = 1, amplitude = 30;
+
+function changeColor(){
+
+    setTimeout(function(){
+
+        if(n >= amplitude){
+            n = 0;
+            increment *= -1;
+        }else{
+            n += 1;
+        }
+
+        for(k = 0; k < 3; k++){
+            
+                darkBlue[k] += increment;
+            
+            lightBlue[k] -= increment;
+        }
+
+        darkBlueText = "rgb(" + darkBlue[0] + "," + darkBlue[1] + "," + darkBlue[2] + ")";
+        lightBlueText = "rgb(" + lightBlue[0] + "," + lightBlue[1] + "," + lightBlue[2] + ")";
+
+        $(".colored").css("background", "linear-gradient(to bottom right, "+darkBlueText+", "+lightBlueText+")");
+
+        //console.log("Change color : " + darkBlueText + " & " + lightBlueText + "     n = " + n);
+
+        changeColor();
+
+    }, 60);
+
+}
+
+changeColor();
+
