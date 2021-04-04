963ac273408106e8cf9ebf68fa1f9589f1453105
Sasha Contrepois <sasha.contrepois@hotmail.fr>
2021-04-04 23:41:50 +0200

    📝 Update : Change the 404 error page interface

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,24 @@
 <template>
 
-    <section class="colored titleSection">
-        <h1>Page non trouvée !</h1>
-        <router-link class="btn btn-secondary btn-outline" to="/">Accueil</router-link>
+    <section class="dividedSection">
+
+        <div class="colored header">
+            <h1>Erreur 404</h1>
+            <h2>Page non trouvée !</h2>
+        </div>
+
+        <div class="body">
+            <div class="container">
+                            
+                <p class="text-center">On dirait bien que la page à laquelle vous essayez d'accéder n'existe pas !<p>
+
+                <div class="d-flex justify-content-center">
+                    <router-link class="btn btn-blue btn-small" to="/"><i class="fas fa-home"></i></router-link>
+                </div>
+
+            </div>
+        </div>
+
     </section>
 
 </template>
