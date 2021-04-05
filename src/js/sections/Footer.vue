cd7be6a8a7b47c1b1463a8997353a5a3ad1866c3
Sasha Contrepois <sasha.contrepois@hotmail.fr>
2021-04-05 22:38:13 +0200

    📝 Update : Change footer

--- src/js/sections/Footer.vue
+++ src/js/sections/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer>
-            <p class="text-center">Développé avec <a href="https://getbootstrap.com/">Bootstrap</a> & <a href="https://v3.vuejs.org/">VueJS</a></p>
+            <p class="text-center">Développé avec <a href="https://getbootstrap.com/" target="_blank">Bootstrap</a> & <a href="https://v3.vuejs.org/" target="_blank">VueJS</a></p>
     </footer>
 </template>
 
