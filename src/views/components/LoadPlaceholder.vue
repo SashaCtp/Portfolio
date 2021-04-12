7545a6fd011b78bc06eb025e0e92f6fa0dbc2bf3
Sasha Contrepois <sasha.contrepois@hotmail.fr>
2021-04-12 19:26:34 +0200

    📝 Update : Add loader and placeholder component

---
+++ src/views/components/LoadPlaceholder.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1 class="placeholder" v-if="this.type === 'h1'" :class="size"></h1>
+        <h2 class="placeholder" v-if="this.type === 'h2'" :class="size"></h2>
+    </div>
+</template>
+
+<script>
+export default{
+    props: {
+        type: String,
+        size: String
+    }
+}
+</script>
