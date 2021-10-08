7400535c2dd42db216f93da9e140703515a1ffac
Sasha Contrepois <sasha.contrepois@hotmail.fr>
2021-10-08 08:34:56 +0200

    📝 Update : Change presentation text

--- src/views/sections/PresentationSection.vue
+++ src/views/sections/PresentationSection.vue
@@ -20,13 +20,18 @@
                                 d'informatique, d'aéronautique et d'ingéniérie.
                     </p>
                     <p>
-                        <span class="tab">Après</span> avoir suivis un an de CPGE, j'ai décidé d'intégrer l'IUT Informatique de Paris afin d'acquérir de solides compétences en Informatique en vu de faire un parcours en apprentissage en école d'ingénieur. Cette première année m'a permis d'acquérir de bonnes méthodes de travail, de la rigueur et de nombreuses connaissances dans les domaines scientifiques, notamment en ingéniérie.
+                        <span class="tab">Je</span> suis actuellement en deuxième année de DUT Informatique à l'IUT de Paris, en alternance à Safran Landing Systems en tant qu'apprenti Ingénieur Outils d'Analyses Systèmes.
+                        <br>
+                         Après avoir suivi une anné de CPGE, j'ai décidé d'intégrer l'IUT de Paris afin d'acquérir de solides compétences en Informatique en vu de faire un parcours en apprentissage. Cette année de CPGE m'a permis d'acquérir de bonnes méthodes de travail, de la rigueur et de nombreuses connaissances dans les domaines scientifiques, notamment en ingéniérie.
                     </p>
                     <p>
-                        <span class="tab">A</span> la sortie de cette école d'ingénieur, je vise de travailler en tant qu'ingénieur dans le secteur de la défense, en cybersécurité ou systèmes embarqués. Vous trouverez ici <a href="https://www.linkedin.com/in/sasha-contrepois/" target="_blank">mon profil LinkedIn</a>.
+                        <span class="tab">Je</span> vise de travailler en tant qu'ingénieur dans le secteur de la défense, en cybersécurité ou systèmes embarqués dans le secteur des transports aériens ou ferroviaires et donc pour cela je souhaite intégrer une école d'ingénieur à la fin de mon cursus de DUT en Septembre 2022. Vous trouverez ici <a href="https://www.linkedin.com/in/sasha-contrepois/" target="_blank">mon profil LinkedIn</a>.
                     </p>
                     <p>
-                        <span class="tab">Je</span> suis quelqu'un de curieux, dynamique, toujours prêt à apprendre de nouvelles choses, et cela depuis tout petit. J'ai commencé à programmer dès l'âge de 13 ans, et depuis, je passe mon temps libre à programmer de petits projets personnels ou de plus grande envergure. Vous pouvez en retrouver quelques'un sur ma page <a href="https://github.com/SashaCtp" target="_blank">GitHub</a>.
+                        <span class="tab">Je</span> suis quelqu'un de curieux, dynamique, toujours prêt à apprendre de nouvelles choses, et cela depuis tout petit. Grâce à de nombreux projets que j'ai pu mené, j'ai acquis des compétences de travail en équipe, de gestion de projet et d'équipe.
+                    </p>
+                    <p>
+                        <span class="tab">J'ai</span> commencé à programmer dès l'âge de 13 ans, et depuis, je passe mon temps libre à programmer de petits projets personnels ou de plus grande envergure. Vous pouvez en retrouver quelques'un sur ma page <a href="https://github.com/SashaCtp" target="_blank">GitHub</a>.
                     </p>
 
                     <div class="row justify-content-center">
@@ -34,90 +39,80 @@
                             <router-link to="parcours" class="btn btn-primary">Mon parcours</router-link>
                         </div>
                     </div>
-							
-                            <table class="table-blue">
 
-                                <thead>
-                                    <tr>
-                                        <td colspan="2">Je recherche actuellement</td>
-                                    </tr>
-                                </thead>
+                </article>
 
-                                <tbody>
+                <div class="col-lg-6 col-12 timeline">
 
-                                    <tr>
-                                        <td colspan="2">Une alternance de 2<span class="text-exp">ème</span> année d'IUT Informatique</td>
-                                    </tr>
+                    <h5>Diplômes et études</h5>
 
-                                    <tr>
-                                        <td class="text-bold">Dans quel secteur ?</td>
-                                        <td>Informatique, défense et systèmes embarqués</td>
-                                    </tr>
-
-                                    <tr>
-                                        <td class="text-bold">Pour quelles dates ?</td>
-                                        <td>Du 1<span class="text-exp">er</span> Septembre 2021 au 31 Août 2022</td>
-                                    </tr>
-
-                                </tbody>
-
-                            </table>
-
-                        </article>
-
-                        <div class="col-lg-6 col-12 timeline">
-
-                            <h5>Diplômes et études</h5>
-
-                            <div class="item item-medium item-red">
-                                <div class="info">
-                                    Depuis Octobre 2020
-                                </div>
-                                Etudiant en 1<span class="text-exp">ère</span> année d'informatique à l'IUT de l'Université de Paris
-                            </div>
-
-                            <div class="divider divider-row">
-                                <div class="bar"></div>
-                                <div class="text">2020</div>
-                                <div class="bar"></div>
-                            </div>
-
-                            <div class="item item-medium item-dark item-border">
-                                <div class="info">
-                                    Classe de PCSI puis PSI - De Sep. 2019 à Oct. 2020
-                                </div>
-                                Etudiant en CPGE au Lycée Chaptal
-                            </div>
-
-                            <div class="item item-medium item-blue item-icon">
-                                <i class="fas fa-graduation-cap"></i>
-
-                                Baccalauréat S - Sciences de l'Ingénieur, Mention TB & Anglais Européen
-                            </div>
-
-                            <div class="divider divider-row">
-                                <div class="bar"></div>
-                                <div class="text">2019</div>
-                                <div class="bar"></div>
-                            </div>
-
-                            <div class="item item-medium item-blue item-icon">
-                                <i class="fas fa-graduation-cap"></i>
-
-                                Brevet d'Initiation à l'Aéronautique - Félicitations du Jury
-                            </div>
-
-                            <div class="divider divider-row">
-                                <div class="bar"></div>
-                                <div class="text">2018</div>
-                                <div class="bar"></div>
-                            </div>
-
+                    <div class="item item-medium item-red">
+                        <div class="info">
+                            Chargé de moderniser des logiciels d'étude de matériaux
                         </div>
+                        
+                        <div class="text">
+                            Apprenti Ingénieur Outils d'Analyses de Structures chez Safran Landing Systems
+                        </div>
+                    </div>
 
+                    <div class="item item-medium item-red">
+                        <div class="info">
+                            Depuis Octobre 2020
+                        </div>
+                        
+                        <div class="text">
+                            Etudiant en 2<span class="text-exp">ème</span> année de DUT Informatique à l'IUT de Paris
+                        </div>
+                    </div>
+
+                    <div class="divider divider-row">
+                        <div class="bar"></div>
+                        <div class="text">2020</div>
+                        <div class="bar"></div>
+                    </div>
+
+                    <div class="item item-medium item-dark item-border">
+                        <div class="info">
+                            Classe de PCSI puis PSI - De Sep. 2019 à Oct. 2020
+                        </div>
+                        <div class="text">
+                            Etudiant en CPGE au Lycée Chaptal
+                        </div>
+                    </div>
+
+                    <div class="item item-medium item-blue item-icon">
+                        <i class="fas fa-graduation-cap"></i>
+
+                        Baccalauréat S - Sciences de l'Ingénieur, Mention TB & Anglais Européen
+                    </div>
+
+                    <div class="divider divider-row">
+                        <div class="bar"></div>
+                        <div class="text">2019</div>
+                        <div class="bar"></div>
+                    </div>
+
+                    <div class="item item-medium item-blue item-icon">
+                        <i class="fas fa-graduation-cap"></i>
+
+                        <div class="text">
+                            Brevet d'Initiation à l'Aéronautique - Félicitations du Jury
+                        </div>
+                    </div>
+
+                    <div class="divider divider-row">
+                        <div class="bar"></div>
+                        <div class="text">2018</div>
+                        <div class="bar"></div>
                     </div>
 
                 </div>
 
-        </section>
+            </div>
+
+        </div>
+
+    </section>
+    
 </template>
--- src/views/sections/SkillSection.vue
+++ src/views/sections/SkillSection.vue
@@ -15,15 +15,7 @@
                 </div>
 
                 <div class="skills" v-if="skills.length != 0">
-
-                    <div class="row">
-
-                        <div class="col-12 col-sm-6 col-lg-3" v-for='(skill, index) in skills' :key='index'>
-                            <skill-card v-bind='skill' />
-                        </div>
-
-                    </div>
-                
+                    <skill-card v-for="(skill, index) in skills" v-bind="skill" :key="index" />
                 </div>
 
         </section>
