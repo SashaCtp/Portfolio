164af682cc679916cc6ee311632e7cb4d30343c9
Sasha Ctp <sasha.contrepois@hotmail.fr>
2022-11-06 21:36:22 +0100

    📝 Update : Add variable BASE_URL environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const BASE_URL = process.env.BASE_URL || '/';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `${BASE_URL}`,
   plugins: [vue()]
-})
+});
